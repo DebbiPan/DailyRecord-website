5825be7b8240ccc6b2f37bc5c5becd83042da753
Debbie <1770554944@qq.com>
2021-08-25 13:23:06 +0800

    deploy

---
+++ precache-manifest.921254856b02c15f72b318187d5653ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49726f47cc583b384685",
+    "url": "/DailyRecord-website/css/app.e0eaa711.css"
+  },
+  {
+    "revision": "8371185961bffd45943dcff404b12db4",
+    "url": "/DailyRecord-website/index.html"
+  },
+  {
+    "revision": "49726f47cc583b384685",
+    "url": "/DailyRecord-website/js/app.7999fde7.js"
+  },
+  {
+    "revision": "c6e8ef63615af9350c0f",
+    "url": "/DailyRecord-website/js/chunk-vendors.46249bd7.js"
+  },
+  {
+    "revision": "903788e72e1163216aa9d7bd6df928f6",
+    "url": "/DailyRecord-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/DailyRecord-website/robots.txt"
+  }
+]);
